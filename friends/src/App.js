08eb1ff68b347048900f2375f5e07a82a8b916b8
Dil Sombayeva <dilyasom@gmail.com>
2019-06-10 12:43:41 -0700

    implement post req

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import ListFriends from './ListFriends';
+import AddFriend from './AddFriend';
 import { Row } from 'reactstrap';
 
 export default class App extends Component {
@@ -9,8 +10,12 @@ export default class App extends Component {
   super();
   this.state = {
    friends: [],
+   name: '',
+   email: '',
+   age: 0,
   };
  }
+
  componentDidMount() {
   axios
    .get('http://localhost:5000/friends')
@@ -20,10 +25,38 @@ export default class App extends Component {
    .catch(err => console.log(err));
  }
 
+ handleChange = e => {
+  this.setState({ [e.target.name]: e.target.value });
+  //   console.log(this.state);
+ };
+
+ handleSubmit = e => {
+  console.log('hi');
+  const newObj = {
+   name: this.state.name,
+   age: this.state.age,
+   email: this.state.email,
+  };
+  axios
+   .post('http://localhost:5000/friends', newObj)
+   .then(res => {
+    // console.log('res', res);
+    this.setState({ friends: res.data });
+   })
+   .catch(err => {
+    console.log(err);
+   });
+ };
+
  render() {
   return (
    <div className="App">
     <h1>Friends</h1>
+    <AddFriend
+     handleChange={this.handleChange}
+     friends={this.state.friends}
+     handleSubmit={this.handleSubmit}
+    />
     <Row className="displayFlex">
      {this.state.friends.map(friend => {
       return <ListFriends key={friend.id} friend={friend} />;
